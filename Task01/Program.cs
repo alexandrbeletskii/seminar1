692dbc54221e678619294cbcb9a87d8fa9be6e59
alex <beletskii1995@mail.ru>
2023-08-06 20:46:28 +1000

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает 2
+// числа и проверяет, является ли первое чилсо квадратом
+// второго
+// a = 25 , b = 5 -> да
+// a = 2 , b = 10 -> нет
+// a = 9 , b = 3 -> да
+// a = 3 , b = 9 -> нет
+
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int square = number1 * number2;
+if (number1/number2 == number2)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
