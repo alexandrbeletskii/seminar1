6a6ccf8c230407875ff0db6bd333094afb663250
alex <beletskii1995@mail.ru>
2023-09-16 01:19:08 +1000

    Task32

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -25,21 +25,22 @@ void FillArray(int[] array)
     Random rnd =    new Random();
     for (int i = 0; i < array.Length; i++)
 {
-    array[i] =  rnd.Next(2);
+    array[i] =  rnd.Next();
    }
 }
+
 
 void PrintArray(int[] array)
 {
     Random rnd =    new Random();
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
 {
        Console.Write(array[i] + " ");
 }
 }
 
 
-int[] arr = new int[8];
+int[] array = new int[8];
 
 FillArray(arr);
 PrintArray(arr);
