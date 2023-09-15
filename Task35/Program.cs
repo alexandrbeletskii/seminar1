cce96fc2d18dde4cfebe06cdf8d275e9cd9262a9
alex <beletskii1995@mail.ru>
2023-09-16 01:21:49 +1000

    Task35

---
+++ Task35/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int CountTowDigitNumber(int[] arr, int min, int max)
+{
+    int count=0;
+     for (int i = 0; i < arr.Length; i++)
+     {
+if (arr[i]>=min && arr[i]<=max) count++;
+     }
+     return count;
+}
+
+
+int[] array = CreateArrayRndInt(123, -100, 123);
+PrintArray(array);
+Console.WriteLine();
+int result = CountTowDigitNumber(array,10,99);
+Console.WriteLine($"Количество чисел в в заданном диапозон равно {result}");
