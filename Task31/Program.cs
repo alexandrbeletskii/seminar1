5bd7a6ba1fe4d6fb04e1381017b2bc8dee46b495
alex <beletskii1995@mail.ru>
2023-09-16 01:20:53 +1000

    Task31

---
+++ Task31/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный
+// случайными числами из промежутка [-9, 9]. Найдите сумму
+// отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма
+// положительных чисел равна 29, сумма отрицательных равна
+// -20.
+
+int [] SumPositiveNegativeElem (int [] arr)
+{
+    int sumPositive=0;
+    int sumNegative=0;
+    for (int i=0; i<arr.Length; i++)
+    {
+        if (arr[i] > 0) sumPositive += arr[i];
+        else sumNegative += arr[i];
+    }
+return new int[] {sumPositive, sumNegative};
+}
+
+
+int SumPositive(int [] arr)
+{
+    int sum=0;
+    for (int i=0; i<arr.Length; i++)
+    {
+        if (arr[i] > 0) sum += arr[i];
+      }
+return sum;
+}
+
+int SumNegative(int [] arr)
+{
+    int sum=0;
+    for (int i=0; i<arr.Length; i++)
+    {
+        if (arr[i] < 0) sum += arr[i];
+           }
+return sum;
+}
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int [] arr = new int[size];
+    Random rnd =    new Random();
+    for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] =  rnd.Next(min, max+1);
+   }
+   return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[") ;
+    for (int i = 0; i < arr.Length; i++)
+{
+      if (i<arr.Length-1) Console.Write ($"{arr[i]}, ");
+      else Console.Write($"{arr[i]}");
+}
+Console.Write("]");
+}
+
+int [] array = CreateArrayRndInt(12,-9,9);
+PrintArray(array);
+Console.WriteLine();
+
+int [] sumPositiveNegativeElem = SumPositiveNegativeElem(array);
+Console.WriteLine($"Сумма положительных элементов равна {sumPositiveNegativeElem[0]}");
+Console.WriteLine($"Сумма отрицательных элементов равна {sumPositiveNegativeElem[1]}");
+
+int sumPositive = SumPositive(array);
+int sumNegative = SumNegative(array);
+
+Console.WriteLine($"Сумма положительных элементов равна {sumPositive}");
+Console.WriteLine($"Сумма отрицательных элементов равна {sumNegative}");
